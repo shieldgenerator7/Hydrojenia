45823fd7df9dffd518df8d9f841de724ee3d8fc4
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-11-05 11:35:32 -0500

    Basic player movement

---
+++ Assets/ShipController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+    public float movementSpeed = 5;
+
+    private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 movementVector = new Vector2(horizontal, vertical).normalized;
+        //Debug.Log("movementVector: " + movementVector * movementSpeed);
+        rb2d.AddForce(movementVector * movementSpeed);
+        Debug.Log("rb2d velcoity: " + rb2d.velocity);
+	}
+}
