0576663461eaf58b0db7141e3d1ef88db3a15719
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-11-05 11:58:03 -0500

    Added rechargable resources

---
+++ Assets/ResourceBar.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceBar : MonoBehaviour
+{
+
+    public float maxResource = 100;
+    [SerializeField]
+    private float currentResource = 0;
+
+    public float ResourceAmount
+    {
+        get { return currentResource; }
+        set
+        {
+            currentResource = value;
+            currentResource = Mathf.Clamp(currentResource, 0, maxResource);
+            Color c = sr.color;
+            c.a = currentResource / maxResource;
+            sr.color = c;
+        }
+    }
+
+    private SpriteRenderer sr;
+
+    // Use this for initialization
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        if (ResourceAmount == 0)
+        {
+            ResourceAmount = maxResource;
+        }
+    }
+
+    public float useResource(float amount)
+    {
+        if (currentResource < amount)
+        {
+            amount = ResourceAmount;
+        }
+        ResourceAmount -= amount;
+        return amount;
+    }
+
+    public void recharge(float amount)
+    {
+        ResourceAmount += amount;
+    }
+}
--- Assets/ShipController.cs
+++ Assets/ShipController.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class ShipController : MonoBehaviour {
 
     public float movementSpeed = 5;
+    public float speedPerFuel = 10;//how much each speed increase costs in fuel
+
+    public float rechargeRate = 5;//how much fuel recharges each second
+
+    public ResourceBar shieldResource;
+    public ResourceBar weaponResource;
+    public ResourceBar engineResource;
 
     private Rigidbody2D rb2d;
 	// Use this for initialization
@@ -13,12 +20,19 @@ public class ShipController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        //Recharge
+        engineResource.recharge(rechargeRate * Time.deltaTime);
+        //Move
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 movementVector = new Vector2(horizontal, vertical).normalized;
-        //Debug.Log("movementVector: " + movementVector * movementSpeed);
-        rb2d.AddForce(movementVector * movementSpeed);
-        Debug.Log("rb2d velcoity: " + rb2d.velocity);
+        Vector2 movementVector = new Vector2(horizontal, vertical);
+        float energy = engineResource.useResource(movementVector.magnitude * movementSpeed / speedPerFuel);
+        energy *= speedPerFuel;
+        if (energy > 0)
+        {
+            rb2d.AddForce(movementVector * energy);
+        }
 	}
 }
