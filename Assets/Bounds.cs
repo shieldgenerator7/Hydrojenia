a2f6a8bca941d76c46d4af180443574a46381008
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-11-05 11:35:49 -0500

    Basic Bounds to keep the player on screen

---
+++ Assets/Bounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GameObject collGO = collision.GetComponent<Collider2D>().gameObject;
+        if (collGO.CompareTag("Player"))
+        {
+            collGO.transform.position = transform.position - (collGO.transform.position - transform.position);
+        }
+    }
+}
