5534449f9abecba3d4b02b6f1317b7165a9b2c6e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-11-05 23:16:57 -0500

    Refactored ShipController resource recharging to be more robust

--- Assets/Ships/ShipController.cs
+++ Assets/Ships/ShipController.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class ShipController : MonoBehaviour {
     
     public float rechargeRate = 5;//how much fuel recharges each second
-    
-    public ResourceBar shieldResource;
-    public ResourceBar weaponResource;
-    public ResourceBar engineResource;
+
+    public List<ResourceBar> resourceBars;
     private ResourceBar rechargeTarget;//the resource that will currently get recharged
+    private int rechargeTargetIndex = 0;//the index of the rechargeTarget in the resourceBars array
     
     private Rigidbody2D rb2d;
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-        rechargeTarget = engineResource;
+        rechargeTarget = resourceBars[rechargeTargetIndex];
 	}
 	
 	// Update is called once per frame
@@ -24,18 +23,14 @@ public class ShipController : MonoBehaviour {
         //Switch recharge target
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            if (rechargeTarget == engineResource)
-            {
-                rechargeTarget = weaponResource;
-            }
-            else
-            {
-                rechargeTarget = engineResource;
-            }
+            rechargeTargetIndex++;
+            rechargeTargetIndex = rechargeTargetIndex % resourceBars.Count;
+            rechargeTarget = resourceBars[rechargeTargetIndex];
         }
         //Recharge
         rechargeTarget.recharge(rechargeRate * Time.deltaTime);
         
+        //Close program
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
